ce14e888db4831e9356c1b57dc6528b01fa4f801
hennasingh <hennasingh04@gmail.com>
2022-03-05 22:42:04 +0000

    added view binding as dependency and updated nav graph, and added binding in Library Activity. replaced fragment with fragment container view.

--- app/src/main/java/com/geek/booklog/LibraryActivity.kt
+++ app/src/main/java/com/geek/booklog/LibraryActivity.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.geek.booklog.databinding.ActivityLibraryBinding
 import io.realm.mongodb.User
 import kotlinx.android.synthetic.main.activity_library.*
 import timber.log.Timber
@@ -14,10 +17,12 @@ import timber.log.Timber
 class LibraryActivity : AppCompatActivity() {
 
     private var user: User? = null
+    private lateinit var binding: ActivityLibraryBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_library)
+        binding = ActivityLibraryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         user = bookLogApp.currentUser()
 
@@ -25,16 +30,20 @@ class LibraryActivity : AppCompatActivity() {
         if (user == null) {
             // if no user is currently logged in, start the login activity so the user can authenticate
             startActivity(Intent(this, LoginActivity::class.java))
+        }else {
+
+            setUpViews()
         }
     }
 
     private fun setUpViews() {
 
         //Finding the Navigation Controller
-        val navController = findNavController(R.id.fragNavHost)
+       val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragNavHost) as NavHostFragment
+        val navController = navHostFragment.navController
 
         // Setting Navigation Controller with the BottomNavigationView
-        bottomNavView.setupWithNavController(navController)
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController)
 
         // Setting Up ActionBar with Navigation Controller
         // Pass the IDs of top-level destinations in AppBarConfiguration
@@ -49,8 +58,4 @@ class LibraryActivity : AppCompatActivity() {
 
     }
 
-    override fun onStart() {
-        super.onStart()
-        setUpViews()
-    }
 }
